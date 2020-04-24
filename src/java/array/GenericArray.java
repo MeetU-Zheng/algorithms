cba51a108ca2127db016e614a4fac9cf7ba98c37
Tfenzheng <839811269@qq.com>
2020-04-24 17:04:54 +0800

    begin...

---
+++ src/java/array/GenericArray.java
@@ -0,0 +1,12 @@
+package java.array;
+
+/*** use array implement LRU algorithm ***/
+
+public class GenericArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
