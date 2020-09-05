35cd23a1daa7593a97250de6c2cd505bbd1ca6cb
Tfenzheng <839811269@qq.com>
2020-09-05 22:17:05 +0800

    2020.09.05

--- src/binary_search/SearchRange.java
+++ src/binary_search/SearchRange.java
@@ -38,9 +38,26 @@ public class SearchRange {
 	//TODO:简化方法1
 	public int[] solution2(int[] nums,int target) {
 		int[] ans = {-1,-1};
-		
+		int start = firstGreaterOrEqual(nums, target);
+		if(start == nums.length || nums[start]!=target)
+			return ans;
+		ans[0]=start;
+		ans[1] = firstGreaterOrEqual(nums, target+1)-1;
 		return ans;
 	}
+	
+	private int firstGreaterOrEqual(int[] nums, int target) {
+		int left = 0,right = nums.length-1;
+		while(left<=right) {
+			int mid = left + (right - left)/2;
+			if(nums[mid] >= target)
+				right=mid-1;
+			else
+				left = mid +1;
+		}
+		return left;
+	}
+	
 	public static void main(String[] args) {
 		int[] n = {5,7,7,8,8,10};
 		new SearchRange().solution1(n, 6);
